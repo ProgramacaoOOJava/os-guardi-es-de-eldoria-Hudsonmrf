c316462bb816846a3f91e2d333129b078025d5c5
Hudsonmrf <91918765+Hudsonmrf@users.noreply.github.com>
2026-06-28 10:50:26 +0000

    Desafio "Nível Notato: Versão inicial com 2 Personagens

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+       
+        Personagem personagem1 = new Personagem("Anita", "Guerreiro", 5, 100, 15.5); // Instacia a classe Personagem na criadção do personagem 1
+        Personagem personagem2 = new Personagem("Hudson", "Mago", 4, 80, 12.5); // Instacia a classe Personagem na criadção do personagem 2
 
-        .
-        .
-        .
+        personagem1.exibirStatus(); // exibe informações do personagem 1
+        personagem2.exibirStatus(); // exibe informações do personagem 2
 
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,26 @@
 public class Personagem {
+   
+    // Atrubutos da classe Personagem
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+    // Construtor para inicialização dos atrubutos
+    public Personagem (String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
+    };
 
-    }
+        // Método que vai exibir informações dos personagens na classe Mais
+    public void exibirStatus (){
+            System.out.println("Nome: " + nome + "\n" + "Classe: " + classe + "\n" + "Nível: " + nivel + "\n" + "Pontos de Vida: " + pontosDeVida + "\n" + "Poder Base: " + poderBase + "\n");
 
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
 
-    }
+    };
 }
 
